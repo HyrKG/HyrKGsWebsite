19f2febe08f80f82045fd770e2f0632c5c331cb1
HyrKG <end_kg@126.com>
2022-09-17 18:51:23 +0800

    初始化仓库，基于vue、vite与element-plus。

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-config-provider namespace="ep">
+
+    <el-container class="operation-wrapper">
+      <el-header class="el-header">
+        <BaseHeader/>
+      </el-header>
+      <el-container>
+        <el-main class="el-main">
+          <div class="el-container">
+            <HelloWorld/>
+          </div>
+
+        </el-main>
+      </el-container>
+    </el-container>
+  </el-config-provider>
+</template>
+
+<style>
+
+.operation-wrapper {
+  width: 100% !important;
+}
+
+.el-header {
+  border-bottom: solid 1px var(--ep-menu-border-color);
+  /*height: 61px;*/
+  /*padding: 0 0;*/
+}
+
+.el-main {
+  padding: 0px 16px !important;
+  height: calc(100vh - 61px);
+  overflow-y: auto;
+}
+
+.el-container {
+  margin: 0 auto;
+  max-width: var(--vp-max-screen-width);
+  text-align: center;
+  color: var(--ep-text-color-primary);;
+}
+
+
+</style>
---
+++ src/components/layouts/BaseHeader.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import {toggleDark} from '~/composables';</script>
+
+<template>
+  <el-menu class="el-menu-demo" mode="horizontal">
+    <!--    <el-menu-item index="1">HyrKG</el-menu-item>-->
+    <img class="logo" alt="logo" src="../../assets/logohyrkg2.png"/>
+
+    <div class="flex-grow"/>
+    <el-menu-item index="1">工单</el-menu-item>
+    <el-sub-menu index="3">
+      <template #title>其他</template>
+      <el-menu-item index="3-1">item one</el-menu-item>
+      <el-menu-item index="3-2">item two</el-menu-item>
+    </el-sub-menu>
+    <el-menu-item index="2">关于我</el-menu-item>
+
+    <el-menu-item h="full" @click="toggleDark()">
+      <button class="border-none w-full bg-transparent cursor-pointer" style="height: var(--ep-menu-item-height);">
+        <i inline-flex i="dark:ep-moon ep-sunny"/>
+      </button>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<style>
+.el-menu-demo {
+  margin: 0 auto;
+  max-width: var(--vp-max-screen-width);
+  border-bottom: none;
+}
+
+.logo {
+  /*position: relative;*/
+  height: calc(var(--ep-menu-item-height) * 0.6);
+  padding: 0 var(--ep-menu-base-level-padding);
+  margin-top: 15px;
+}
+</style>
