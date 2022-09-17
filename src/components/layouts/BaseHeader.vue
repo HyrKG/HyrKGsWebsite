42ca54f002a3e278979513bf5e632b4a557b0e46
HyrKG <end_kg@126.com>
2022-09-17 22:25:58 +0800

    在暗夜模式下显示暗夜模式的LOGO

--- src/components/layouts/BaseHeader.vue
+++ src/components/layouts/BaseHeader.vue
@@ -1,20 +1,20 @@
 <script lang="ts" setup>
-import {toggleDark} from '~/composables';</script>
+import {isDark, toggleDark} from '~/composables';</script>
 
 <template>
   <el-menu class="el-menu-demo" mode="horizontal">
     <!--    <el-menu-item index="1">HyrKG</el-menu-item>-->
-    <img class="logo" alt="logo" src="../../assets/logohyrkg2.png"/>
+
+    <div v-if="isDark">
+      <img class="logo" alt="logo" src="../../assets/logo-dark.png"/>
+    </div>
+    <div v-else>
+      <img class="logo" alt="logo" src="../../assets/logo-light.png"/>
+    </div>
 
     <div class="flex-grow"/>
     <el-menu-item index="1">工单</el-menu-item>
-    <el-sub-menu index="3">
-      <template #title>其他</template>
-      <el-menu-item index="3-1">item one</el-menu-item>
-      <el-menu-item index="3-2">item two</el-menu-item>
-    </el-sub-menu>
     <el-menu-item index="2">关于我</el-menu-item>
-
     <el-menu-item h="full" @click="toggleDark()">
       <button class="border-none w-full bg-transparent cursor-pointer" style="height: var(--ep-menu-item-height);">
         <i inline-flex i="dark:ep-moon ep-sunny"/>
